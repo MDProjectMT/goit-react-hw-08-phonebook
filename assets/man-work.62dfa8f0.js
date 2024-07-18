3f1ef332cc50211298a70ac3bd4001e6e3980231
MDProjectMT <md.monikatajchert@gmail.com>
2024-07-18 14:45:06 +0200

    Updates

---
+++ assets/man-work.62dfa8f0.js
@@ -0,0 +1,1 @@
+const o="/goit-react-hw-08-phonebook/assets/man-work.da60ae07.jpg";export{o as w};
---
+++ assets/workplace.4a682ac1.js
@@ -0,0 +1,1 @@
+const e="/goit-react-hw-08-phonebook/assets/workplace.e782fd21.jpg";export{e as w};
